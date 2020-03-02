98aec009e6bca4c553d68b59b8e8518f63cae207
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-03-02 21:01:43 +0900

    Disable coverage

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'node', 'json'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   coveragePathIgnorePatterns: ['/node_modules/', '\\.d\\.ts$', '\\.(spec|test)\\.ts$'],
   globals: {
