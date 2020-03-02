06bf2a48843413d3dc4e1f632dd40e3e75c4eef9
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-03-02 20:48:25 +0900

    Disable some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/class-name-casing': 0,
+    '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/member-delimiter-style': [
       'error',
       {
