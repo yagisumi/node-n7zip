cba1d51c7eca0ab3fc590cc98dbf16b4dfad0c32
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-03-08 01:30:15 +0900

    Include MyInitGuid.h

--- ext/n7zip/guid.cpp
+++ ext/n7zip/guid.cpp
@@ -1,5 +1,5 @@
-// #include <Common/MyInitGuid.h>
-#define INITGUID
+#include <Common/MyInitGuid.h>
+// #define INITGUID
 #include <7zip/Archive/IArchive.h>
 #include <7zip/IStream.h>
 #include <7zip/ICoder.h>
