a022fd7b714b2dc0149e160595c6f1c3161c2e6a
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-10-23 12:16:44 +0900

    Remove unused macro definition

--- ext/n7zip/guid.h
+++ ext/n7zip/guid.h
@@ -7,31 +7,4 @@
 
 CLSID_7ZIP(ICompressCodecsInfo, 0x04, 0x60);
 CLSID_7ZIP(IInStream, 0x03, 0x03);
-
-#define CLSID_FORMAT(name, value)  \
-  DEFINE_GUID(CLSID_CFormat##name, \
-              0x23170F69,          \
-              0x40C1,              \
-              0x278A,              \
-              0x10,                \
-              0x00,                \
-              0x00,                \
-              0x01,                \
-              0x10,                \
-              value,               \
-              0x00,                \
-              0x00)
-
-CLSID_FORMAT(Zip, 0x01);
-// CLSID_FORMAT(BZip2, 0x02);
-// CLSID_FORMAT(Rar, 0x03);
-// CLSID_FORMAT(Z,   0x05);
-// CLSID_FORMAT(Lzh, 0x06);
-// CLSID_FORMAT(7z,  0x07);
-// CLSID_FORMAT(Cab, 0x08);
-// CLSID_FORMAT(Lzma,0x0A);
-
-// CLSID_FORMAT(Wim, 0xE6);
-// CLSID_FORMAT(Iso, 0xE7);
-// CLSID_FORMAT(Tar, 0xEE);
-// CLSID_FORMAT(GZip,0xEF);
+CLSID_7ZIP(IInArchive, 0x06, 0x60);
