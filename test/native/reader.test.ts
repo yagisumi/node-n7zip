559ba16f560e7dd72f86897508c748efd8074966
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-10-26 03:37:40 +0900

    Add test

--- test/native/reader.test.ts
+++ test/native/reader.test.ts
@@ -1,12 +1,25 @@
-import { n7zip_native } from '@/n7zip_native'
+import { n7zip_native, InStreamData } from '@/n7zip_native'
 import path from 'path'
 
-const file = path.resolve(__dirname, '../files/in_stream.txt')
+const file = path.resolve(__dirname, '../files/archive.zip')
 
 describe('Reader', () => {
-  test.only('createReader', () => {
+  test('createReader', () => {
     const formats = n7zip_native.getFormats()
-    const fmtIndices = formats.filter((fmt) => ['zip'].includes(fmt.name)).map((fmt) => fmt.index)
-    console.log(n7zip_native.createReader([['in_stream.txt', file]], fmtIndices))
+    console.log(formats.filter((fmt) => ['zip', '7z', '7zip'].includes(fmt.name)))
+    // console.log(formats)
+    const fmtIndices = formats
+      // .filter((fmt) => ['zip', '7z'].includes(fmt.name))
+      .filter((fmt) => ['zip'].includes(fmt.name))
+      .map((fmt) => fmt.index)
+    const files: InStreamData[] = [
+      // ['archive.7z.001', path.resolve(__dirname, '../files/archive.7z.001')],
+      // ['archive.7z.002', path.resolve(__dirname, '../files/archive.7z.002')],
+      ['archive_rar.z01', path.resolve(__dirname, '../files/archive_rar.z01')],
+      ['archive_rar.z02', path.resolve(__dirname, '../files/archive_rar.z02')],
+      ['archive_rar.zip', path.resolve(__dirname, '../files/archive_rar.zip')],
+    ]
+    // const files = [['in_stream.txt', file]]
+    console.log(n7zip_native.createReader(files, fmtIndices))
   })
 })
